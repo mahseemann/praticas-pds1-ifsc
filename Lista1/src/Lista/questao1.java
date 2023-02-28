3c00f33d77cf637952d4b13dedce2b4e3830166f
Maiara <maiarajunchesseemann@gmail.com>
2023-02-28 09:07:51 -0300

    Lista1

---
+++ Lista1/src/Lista/questao1.java
@@ -0,0 +1,18 @@
+package Lista;
+
+public class questao1 {
+
+	public static void main(String[] args) {
+
+		String precoL = JOptionPane.showInputDialog(null,"Valor do preço por litro:");
+		String quantL = JOptionPane.showInputDialog(null, "Quantidade em litros:");
+		
+		Double precoL = Double.valueOf(precoL);
+		Integer quantL = Integer.valueOf(quantL);
+		
+	}
+	
+	public static void totalPagamento (Double precoL, Integer quantL) {
+		
+	}
+}
